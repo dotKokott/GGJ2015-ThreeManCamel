86b2946057c26a5dadfe187ac3525c71cd4254b0
Mattia Traverso <mattiaballa@gmail.com>
2015-01-23 23:53:28 +0100

    Added Basic controller class
    Reminder: talk with C&C about time step

---
+++ TMC/Assets/Controls.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CharType {
+	Warrior,
+	Mage,
+	Defender,
+	Healer
+}
+
+public class Controls : MonoBehaviour {
+	//topspeed = ACCEL/FRICTION
+	private Vector3 velocity = new Vector3();
+	public float acceleration;
+	public float friction;
+
+	public CharType Type;
+
+	Rigidbody rigidbody;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+
+		//Do in editor to "fix" bounciness
+		//Time.fixedTime = 0.015f;
+	}
+
+	void Update () {
+		velocity.x += acceleration * Time.deltaTime * Input.GetAxis ("Horizontal") - velocity.x * friction * Time.deltaTime;
+		velocity.z += acceleration * Time.deltaTime * Input.GetAxis ("Vertical") - velocity.z * friction * Time.deltaTime;
+
+		rigidbody.MovePosition(transform.position + velocity);
+	}
+}
