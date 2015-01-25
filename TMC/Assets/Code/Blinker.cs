6e0c8ac2cab6ba94d9181b1f8da1b39bd14cc55f
Mattia Traverso <mattiaballa@gmail.com>
2015-01-25 13:49:37 +0100

    those fuckers are out of the way

--- TMC/Assets/Code/Blinker.cs
+++ TMC/Assets/Code/Blinker.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[DisallowMultipleComponent]
+[DisallowMultipleComponentsAttribute]
 public class Blinker : MonoBehaviour {
 
 	// Use this for initialization
