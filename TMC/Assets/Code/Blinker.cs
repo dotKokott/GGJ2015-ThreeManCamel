c8f71992408671fbece9af6529482765e214a3f9
Mattia Traverso <mattiaballa@gmail.com>
2015-01-27 18:21:34 +0100

    Faster Blinker / Fixed level 3 UI

--- TMC/Assets/Code/Blinker.cs
+++ TMC/Assets/Code/Blinker.cs
@@ -15,13 +15,13 @@ public class Blinker : MonoBehaviour {
         foreach ( var item in r ) {
             item.enabled = false;
         }
-        yield return new WaitForSeconds( 0.02f );
+        yield return new WaitForSeconds( 0.01f );
         foreach ( var item in r ) {
             item.enabled = true;
         }
 
         if ( times > 0 ) {
-            yield return new WaitForSeconds( 0.1f );
+            yield return new WaitForSeconds( 0.08f );
             StartCoroutine( Blink( times - 1 ) );
         } else {
             Destroy( gameObject );
