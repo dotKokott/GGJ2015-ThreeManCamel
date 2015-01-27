975735f2c7374cab773de82077af616001866213
Christian Kokott <christian.kokott@googlemail.com>
2015-01-27 17:12:11 +0100

    added backup prompts

---
+++ TMC/Assets/Code/YSwapper.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum PromptType {
+    Y,
+    A,
+    Back
+}
+
+public class YSwapper : MonoBehaviour {
+
+    public Sprite controllerY;
+    public Sprite controllerA;
+    public Sprite controllerRestart;
+
+    public Sprite keyboardY;
+    public Sprite keyboardA;
+    public Sprite keyboardRestart;
+
+    public Image image;
+
+    public PromptType Type;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var controllerPluggedin = Input.GetJoystickNames().Length > 0;
+
+        if (controllerPluggedin) {
+            switch (Type) {
+                case PromptType.Y:
+                    image.sprite = controllerY;
+                    break;
+                case PromptType.A:
+                    image.sprite = controllerA;
+                    break;
+                case PromptType.Back:
+                    image.sprite = controllerRestart;
+                    break;
+                default:
+                    break;
+            }
+            
+        } else {
+            switch (Type) {
+                case PromptType.Y:
+                    image.sprite = keyboardY;
+                    break;
+                case PromptType.A:
+                    image.sprite = keyboardA;
+                    break;
+                case PromptType.Back:
+                    image.sprite = keyboardRestart;
+                    break;
+                default:
+                    break;
+            }
+        }
+	}
+}
